54d943878b5bcc986be630c3f97ff38a394cd31c
yangqiuming <yangqiuming@gmail.com>
2023-04-19 12:50:25 +0800

    update

--- chap-34/test.cpp
+++ chap-34/test.cpp
@@ -3,9 +3,9 @@
 #include "lib/tcp_server.h"
 
 int main() {
-    int thread_num = 2;
+    int thread_num = 4;
     int listen_port = 43211;
-    networking::TcpServer thread_pool(thread_num, listen_port);
-    thread_pool.Start();
+    networking::TcpServer tcp_server(thread_num, listen_port);
+    tcp_server.Start();
     return 0;
 }
